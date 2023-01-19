21b6295f8b17240fa5ccd080e67f5000d4950513
Timothy Ingledue <timothy.ingledue@orion.com>
2023-01-19 17:13:38 -0600

    Everything now working...just need to format table within modal...

---
+++ MudBlazorServer/Models/Inventory.cs
@@ -0,0 +1,9 @@
+﻿namespace MudBlazorServer.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public string Item { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
