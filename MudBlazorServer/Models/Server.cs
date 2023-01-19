80f700c944366b39c12e414b9bb7c4bde0f13eec
Timothy Ingledue <timothy.ingledue@orion.com>
2023-01-19 10:31:00 -0600

    use cases:
    1). Templating and Passing Simple Data
    2). Passing Server Data and and Delete

---
+++ MudBlazorServer/Models/Server.cs
@@ -0,0 +1,10 @@
+﻿namespace MudBlazorServer.Models
+{
+    public class Server
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Location { get; set; } = null!;
+        public string IpAddress { get; set; } = null!;
+    }
+}
